633586d4ab95ed09193b32179cf35415bd48f3d9
Cheulong Sear <searcheulong@gmail.com>
2025-04-13 15:56:31 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="tag">Beta1.0</div>
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
