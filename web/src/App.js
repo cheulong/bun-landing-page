ddd6bd449ee869272eaf7e3a4ee70a0682f7cad3
Cheulong Sear <searcheulong@gmail.com>
2025-04-15 02:48:24 +0700

    add db and prod env

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [version, setVersion] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/version")
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => setVersion(data.msg));
+  }, []);
+
   return (
     <div className="App">
-      <div className="tag">Beta1.0</div>
+      <div className="tag">Beta {version}</div>
       <Home />
       <Footer />
     </div>
--- web/src/Components/Home.js
+++ web/src/Components/Home.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BannerBackground from "../Assets/home-banner-background.png";
 import Bun from "../Assets/bun.png";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubTitle] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/title")
+      .then((response) => response.json())
+      .then((data) => setTitle(data.msg));
+    fetch("http://localhost:3001/subtitle")
+      .then((response) => response.json())
+      .then((data) => setSubTitle(data.msg));
+  }, []);
+
   return (
     <div className="home-container">
       <Navbar />
@@ -13,13 +25,8 @@ const Home = () => {
           <img src={BannerBackground} alt="" />
         </div>
         <div className="home-text-section">
-          <h1 className="primary-heading">
-            Your Favourite Food Delivered Hot & Fresh
-          </h1>
-          <p className="primary-text">
-            Healthy switcher chefs do all the prep work, like peeding, chopping
-            & marinating, so you can cook a fresh food.
-          </p>
+          <h1 className="primary-heading">{title || "Title"}</h1>
+          <p className="primary-text">{subtitle || "Subtitle"}</p>
           <button className="secondary-button">
             Order Now <FiArrowRight />{" "}
           </button>
